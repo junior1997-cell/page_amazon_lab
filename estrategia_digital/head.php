f7918a7a9d032bf47ccf775238e103ec1e511b1c
David Requejo Santacruz <davidrequejo@upeu.edu.pe>
2022-12-26 23:37:14 -0500

    cambio de tipo de letras hermelinda

---
+++ estrategia_digital/head.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Mirrored from htmlstream.com/front/landing-classic-advertisement.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 19 May 2021 14:17:31 GMT -->
+    <head>
+        <!-- Title -->
+        <title>V5| Estrategia Digital</title>
+
+        <!-- Required Meta Tags Always Come First -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="../assets/svg/icons/icon-v5.svg" />
+
+        <!-- Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&amp;display=swap" rel="stylesheet" />
+
+        <!-- CSS Implementing Plugins -->
+        <link rel="stylesheet" href="../assets/css/vendor.min.css" />
+
+        <!-- CSS Implementing Plugins -->
+        <link rel="stylesheet" href="../assets/estrategia_digital/css/ED_v5.css" />
+
+        <!-- CSS Front Template -->
+        <link rel="stylesheet" href="../assets/css/theme.minc619.css?v=1.0" />
+        <style type="text/css">
+
+            @font-face {
+            font-family: 'Panton-SemiBold';
+            src:     url('Panton-SemiBold.otf')  format('truetype'); /* Safari, Android, iOS */
+            }
+            @font-face {
+            font-family: 'Panton-Light';
+            src:     url('Panton-Light.otf')  format('truetype'); /* Safari, Android, iOS */
+
+            }
+            @font-face {
+            font-family: 'Panton-Regular';
+            src:     url('Panton-Regular.otf')  format('truetype'); /* Safari, Android, iOS */
+
+            }
+            .titulos{
+              font-family: Panton-SemiBold;
+            }
+            .contenido{
+              font-family: Panton-Light;
+            }
+
+            .hover_morado {
+                color: white !important;
+            }
+            .hover_morado:hover {
+                color: #583588 !important;
+            }
+            .morado_active {
+                color: #583588 !important;
+            }
+
+        </style>
+    </head>
---
+++ estrategia_digital/header.php
@@ -0,0 +1,89 @@
+
+    <body class="body">
+        <!-- ========== HEADER ========== -->
+        <header
+            id="header"
+            class="header header-box-shadow-on-scroll header-bg-transparent header-abs-top header-show-hide titulos"
+            data-hs-header-options='{
+              "fixMoment": 1000,
+              "fixEffect": "slide"
+            }' >
+            <div class="header-section" style="background-color: #F4B222;">
+                <div id="logoAndNav" class="container">
+                    <!-- Nav -->
+                    <nav class="js-mega-menu navbar navbar-expand-lg">
+                        <!-- Logo -->
+                        <a class="navbar-brand" href="index.php" aria-label="Front">
+                            <img src="../assets/estrategia_digital/svg/logos/busines-wassi-estrategia-digital.svg" alt="Logo" />
+                        </a>
+                        <!-- End Logo -->
+
+                        <!-- Responsive Toggle Button -->
+                        <button type="button" class="navbar-toggler btn btn-icon btn-sm rounded-circle" aria-label="Toggle navigation" aria-expanded="false" aria-controls="navBar" data-toggle="collapse" data-target="#navBar">
+                            <span class="navbar-toggler-default">
+                                <svg width="14" height="14" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        fill="currentColor"
+                                        d="M17.4,6.2H0.6C0.3,6.2,0,5.9,0,5.5V4.1c0-0.4,0.3-0.7,0.6-0.7h16.9c0.3,0,0.6,0.3,0.6,0.7v1.4C18,5.9,17.7,6.2,17.4,6.2z M17.4,14.1H0.6c-0.3,0-0.6-0.3-0.6-0.7V12c0-0.4,0.3-0.7,0.6-0.7h16.9c0.3,0,0.6,0.3,0.6,0.7v1.4C18,13.7,17.7,14.1,17.4,14.1z"
+                                    />
+                                </svg>
+                            </span>
+                            <span class="navbar-toggler-toggled">
+                                <svg width="14" height="14" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        fill="currentColor"
+                                        d="M11.5,9.5l5-5c0.2-0.2,0.2-0.6-0.1-0.9l-1-1c-0.3-0.3-0.7-0.3-0.9-0.1l-5,5l-5-5C4.3,2.3,3.9,2.4,3.6,2.6l-1,1 C2.4,3.9,2.3,4.3,2.5,4.5l5,5l-5,5c-0.2,0.2-0.2,0.6,0.1,0.9l1,1c0.3,0.3,0.7,0.3,0.9,0.1l5-5l5,5c0.2,0.2,0.6,0.2,0.9-0.1l1-1 c0.3-0.3,0.3-0.7,0.1-0.9L11.5,9.5z"
+                                    />
+                                </svg>
+                            </span>
+                        </button>
+                        <!-- End Responsive Toggle Button -->
+                        <!-- Navigation -->
+                        <div id="navBar" class="collapse navbar-collapse">
+                            <div class="navbar-body header-abs-top-inner">
+                                <ul class="navbar-nav">
+                                    <!-- Home -->
+                                    <li class="navbar navbar-light">
+                                        <a id="pagesMegaMenu" href="index.php" aria-haspopup="true" aria-expanded="false" class=" nosotros hover_morado morado_active" onclick="add_quit_clss_header('nosotros')"><b>Nosotros</b></a>
+                                        <!-- Pages - Submenu -->
+                                        <div id="pagesSubMenu" class="hs-sub-menu dropdown-menu" aria-labelledby="pagesMegaMenu" style="min-width: 30px;"></div>
+                                        <div class="col-lg-6"></div>
+                                    </li>
+
+                                    <!-- Docs -->
+                                    <li class="navbar navbar-white">
+                                        <a id="pagesMegaMenu" href="servicios.php" aria-haspopup="true" aria-expanded="false" class="servicios hover_morado text-white" onclick="add_quit_clss_header('servicios')"><b>Servicios</b></a>
+                                        <!-- Pages - Submenu -->
+                                        <div id="pagesSubMenu" class="hs-sub-menu dropdown-menu" aria-labelledby="pagesMegaMenu" style="min-width: 30px;"></div>
+                                        <div class="col-lg-6"></div>
+                                    </li>
+                                    <!-- End Docs -->
+
+                                    <!-- Pages -->
+                                    <li class="navbar navbar-white">
+                                        <a id="pagesMegaMenu" href="contactanos.php" aria-haspopup="true" aria-expanded="false" class="contactanos hover_morado text-white" onclick="add_quit_clss_header('contactanos')"><b>Contáctanos</b></a>
+                                        <!-- Pages - Submenu -->
+                                        <div id="pagesSubMenu" class="hs-sub-menu dropdown-menu" aria-labelledby="pagesMegaMenu" style="min-width: 230px;"></div>
+                                        <div class="col-lg-6"></div>
+                                        <!-- End Pages - Submenu -->
+                                    </li>
+                                    <!-- Pages -->
+                                    <li class="navbar navbar-white">
+                                        <a id="pagesMegaMenu" href="#" aria-haspopup="true" aria-expanded="false" class="hover_morado text-white" onclick="add_quit_clss_header('cursos')"><b>Cursos</b></a>
+                                        <!-- Pages - Submenu -->
+                                        <div id="pagesSubMenu" class="hs-sub-menu dropdown-menu" aria-labelledby="pagesMegaMenu" style="min-width: 230px;"></div>
+                                        <div class="col-lg-6"></div>
+                                        <!-- End Pages - Submenu -->
+                                    </li>
+                                    <!-- End Pages -->
+                                    <!-- <button type="button" class="btn btn-dark">Cursos</button> -->
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- End Navigation -->
+                    </nav>
+                    <!-- End Nav -->
+                </div>
+
+        </header>
+        <!-- ========== END HEADER ========== -->
