0dcf516430a2aa6b0c720ba825e978a71f58e739
Lewis Caro <lewiscaro@upeu.edu.pe>
2022-12-29 03:36:36 -0500

    cambio5

--- assets/consultoria/js/estilo.js
+++ assets/consultoria/js/estilo.js
@@ -15,3 +15,46 @@ $('#switch').on('click', function () {
     } 
 });
 
+var cont =1;
+let conteo = setInterval(()=> {    
+    $('.login-page').css({'background-image':`url('../dist/img/${cont}fondo-login.jpg')`});
+    cont++;
+    if (cont == 5){
+        cont =0;
+    }
+
+}, 8000)
+
+function active(name) {
+
+    if (name == 'inicio') {
+      $('.inicio').addClass("_active"); $('.nosotros').removeClass("_active"); $('.servicios').removeClass("_active"); $('.contactanos').removeClass("_active");
+    } else if (name == 'nosotros') {
+      $('.nosotros').addClass("_active"); $('.inicio').removeClass("_active"); $('.servicios').removeClass("_active"); $('.contactanos').removeClass("_active");
+    } else if (name == 'servicios') {
+      $('.servicios').addClass("_active"); $('.nosotros').removeClass("_active"); $('.inicio').removeClass("_active"); $('.contactanos').removeClass("_active");
+    } else if (name == 'contactanos') {
+      $('.contactanos').addClass("_active"); $('.nosotros').removeClass("_active"); $('.servicios').removeClass("_active"); $('.inicio').removeClass("_active");
+  
+    }
+}
+  
+function myFunction() {
+var dots = document.getElementById("dots");
+var moreText = document.getElementById("more");
+var btnText = document.getElementById("myBtn");
+
+if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Leer más";
+    moreText.style.display = "none";
+} else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Leer menos";
+    moreText.style.display = "inline";
+}
+}
+  
+  
+  
+  
