0ed0a4827d62ac88a294647646b7611efb770a33
lissethuaman <lissethuaman@upeu.edu.pe>
2023-01-05 17:44:26 -0500

    cambio ultimitooooooo

--- estrategia_digital/footer.php
+++ estrategia_digital/footer.php
@@ -3,9 +3,9 @@
             <div class="container" style=" padding: 2rem!important;">
                 <div class="row">
 
-                    <div class="col-12 col-md-12 col-sm-12 col-lg-9 col-xl-9">
-                        <span><i class="fas fa-location-arrow"></i> Jr. Los Helechos 214 - Urb. Bernabe Guridi.</span>
-                        <span><a href="https://api.whatsapp.com/send?phone=944411328&amp;text=" style=" color: #F4B222; text-decoration: none; background-color: transparent;"><i class="fab fa-whatsapp"></i> Whatsapp: +51 944 411 328</a></span>
+                    <div class="col-12 col-md-12 col-sm-12 col-lg-9 col-xl-9 contenido">
+                        <span style="margin-right: 10px;"><i class="fas fa-location-arrow"></i> Jr. Los Helechos 214 - Urb. Bernabe Guridi.</span>
+                        <span><a href="https://api.whatsapp.com/send?phone=944411328&amp;text=" style="color: #F4B222; text-decoration: none; background-color: transparent;"><i class="fab fa-whatsapp"></i>Whatsapp: +51 944 411 328</a></span>
                     </div>
                     <div class="col-12 col-md-12 col-sm-12 col-lg-3 col-xl-3" style="text-align: end;">
                         <div class="redes">
@@ -18,7 +18,7 @@
                     <div class="clearfix"></div>
                 </div>
                 <hr class="opacity-xs my-0" />
-                <div class="text-center" style=" font-size: 14px; padding-top: 15px;" >
+                <div class="text-center contenido" style=" font-size: 14px; padding-top: 15px;" >
                     <strong id="copyright">Copyright &copy; 2022 - 
                     <script>document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))</script> 
                     <a href="https://www.facebook.com/profile.php?id=100086343481837" style="text-decoration: none; color: #F4B222;"> JDL Technology</a>.</strong>
